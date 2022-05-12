241d77beea0f813729b67e2276170ed3dd52353a
Omar jadouani <81360373+Omar-Jadouani@users.noreply.github.com>
2022-05-12 23:48:36 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+$("#logo").on('click', function(){
+
+    window.location = "file:///C:/Users/dell/Desktop/site/TEST/index.html";    
+});
+$("#about").on('click', function(){
+
+    $(document).ready(function(){
+        $("#about").click(function(){
+            window.location = "file:///C:/Users/dell/Desktop/site/TEST/content/about.html";
+
+            // window.location = "file:///C:/Users/dell/Desktop/site/TEST/content/load.html";
+            // $(".loading").css("display", "none");
+            // function myFunction() {
+            // };
+            // myFunction();
+        });
+    });    
+});
+$("#testimonials").on('click', function(){
+    window.location = "file:///C:/Users/dell/Desktop/site/TEST/content/testimonials.html";    
+});
+$("#blog").on('click', function(){
+    window.location = "file:///C:/Users/dell/Desktop/site/TEST/content/blog.html";    
+});
+$("#contact").on('click', function(){
+    window.location = "file:///C:/Users/dell/Desktop/site/TEST/content/contact.html";    
+});
+
+// if(document.getElementById('bar').clicked == true && $("#back-lats-b").on('click', function(){
+        
+//     {
+//         window.location = "file:///C:/Users/dell/Desktop/site/TEST/content/testimonials.html";
+//     }
+// }));
